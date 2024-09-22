29613a2e2bc62e6eb6a1fd05f41b4a0e99d10746
dav <usr21p1@gmail.com>
2024-09-21 22:16:52 -0700

    Fix example bump to v0.2

--- examples/polygot.rs
+++ examples/polygot.rs
@@ -12,27 +12,22 @@ fn main() {
     gen.add_type::<OtherStruct>();
 
     let languages = &[
-        // Language::Typescript(TypescriptOptions {
-        //     prefer_unions: true,
-        //     ..Default::default()
-        // }),
-        Language::JsonSchema(Default::default()),
-        // Language::CSharp(Default::default()),
-        // Language::Crystal(Default::default()),
-        Language::Dart(DartOptions {
-            part_name: "Hello".to_owned(),
-            from_map: true,
+        Language::Typescript(TypescriptOptions {
+            prefer_unions: true,
             ..Default::default()
-
         }),
-        // Language::Elm(Default::default()),
-        // Language::Go(Default::default()),
-        // Language::Haskell(Default::default()),
-        // Language::Python(Default::default()),
-        // Language::Ruby(Default::default()),
-        // Language::Rust(Default::default()),
-        // Language::Smithy(Default::default()),
-        // Language::Swift(Default::default()),
+        Language::JsonSchema(Default::default()),
+        Language::CSharp(Default::default()),
+        Language::Crystal(Default::default()),
+        Language::Dart(Default::default()),
+        Language::Elm(Default::default()),
+        Language::Go(Default::default()),
+        Language::Haskell(Default::default()),
+        Language::Python(Default::default()),
+        Language::Ruby(Default::default()),
+        Language::Rust(Default::default()),
+        Language::Smithy(Default::default()),
+        Language::Swift(Default::default()),
     ];
 
     for lang in languages {
